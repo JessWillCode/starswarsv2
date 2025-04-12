2e0575b953183b3a58aedff5a439cd0a73a03ed5
Jess Williams <jesswillcode@gmail.com>
2025-04-12 12:36:22 -0400

    attempting to add a fallback error since some images are giving us a 404

--- src/CharList.jsx
+++ src/CharList.jsx
@@ -1,10 +1,20 @@
-function CharList({chars, setFave}) {
+import fallback from './assets/fallback.png';
+import {useState} from "react";
+
+function CharList({chars, setFave, fallbackSrc = fallback.src}) {
+    const [imgError, setImgError] = useState(false);
+
     return (
         <><h1>Char List</h1>
             {
                 chars.map((char) =>
                     <div key={char.id}>
-                        <img src={char.image} alt="char" style={{ height: '400px' }} />
+                        <img
+                            src={imgError ? fallbackSrc : char.image}
+                            alt="character picture"
+                            style={{ height: '400px' }}
+                            onError={() => setImgError(true)}
+                        />
                         <h2>{char.name}</h2>
                         <button onClick={() => setFave(char)}>
                             Favorite
